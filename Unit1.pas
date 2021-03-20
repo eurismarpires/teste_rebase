a7c70d633492a65102f740051f99cb09dfea7237
Eurismar Pires Borges <eurismarpires@gmail.com>
2021-03-20 03:36:16 -0300

    criado a mensage de boa noite

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +22,10 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+   ShowMessage('boa noite!');
+end;
 
 end.
